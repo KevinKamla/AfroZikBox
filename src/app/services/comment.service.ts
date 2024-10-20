be54d6286be0cb00cd6ef5fe0d36cc173bbb2cbc
Martialmonjoh <115487309+Martialmonjoh@users.noreply.github.com>
2024-10-20 12:31:23 +0100

    3.1

---
+++ src/app/services/comment.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+
+  private getcomment = `${environment.api}/get-comment`;
+  private registercomment = `${environment.api}/register-comment`;
+  private unlikecomment = `${environment.api}/unlike-comment`;
+
+  private accessToken = localStorage.getItem('accessToken');
+  private serverKey = environment.server_key;
+  constructor(private http: HttpClient) { }
+
+  getComments(TrackId: number): Observable<any> {
+    const params = new HttpParams()
+      .set('server_key', this.serverKey)
+      .set('access_token', this.accessToken || '')
+      .set('id',TrackId)
+      .set('offset',5)
+      .set('limit',3)
+    return this.http.get<any>(this.getcomment, { params });
+  }
+  getRegisterComments(TrackId: number,TimePercentage:number,Value:number): Observable<any> {
+    const params = new HttpParams()
+      .set('server_key', this.serverKey)
+      .set('access_token', this.accessToken || '')
+      .set('id',TrackId)
+      .set('timePercentage',TimePercentage)
+      .set('value',Value)
+    return this.http.get<any>(this.registercomment, { params });
+  }
+  getUnlikeComments(id: number,text:number): Observable<any> {
+    const params = new HttpParams()
+      .set('server_key', this.serverKey)
+      .set('access_token', this.accessToken || '')
+      .set('id',id)
+      .set('comment_description',text)
+    return this.http.get<any>(this.registercomment, { params });
+  }
+  
+}
