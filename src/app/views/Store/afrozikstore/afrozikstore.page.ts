72fbbe221b4b955b8b25bab6844b3dc5f1d16ef7
kevin kamla <kvnkamla4@gmail.com>
2024-11-06 22:56:04 +0100

    'zikStore'

--- src/app/views/Store/afrozikstore/afrozikstore.page.ts
+++ src/app/views/Store/afrozikstore/afrozikstore.page.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AfrozikstorePage implements OnInit {
 
+  
+  selectedSegment = 'chansons';
   constructor() { }
 
   ngOnInit() {
+    this
   }
 
 }
